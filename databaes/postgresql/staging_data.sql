060b0093cdad0ae1509136c4e7f0bd4a62893cf2
basel <baselnabil@gmail.com>
2024-10-03 22:19:15 +0300

    ading readme and optimizing databse

--- databaes/postgresql/staging_data.sql
+++ databaes/postgresql/staging_data.sql
@@ -1,4 +1,4 @@
-CREATE TABLE job_data (
+CREATE TABLE staging_data (
     work_year INT,
     experience_level VARCHAR(50),
     employment_type VARCHAR(50),
@@ -9,10 +9,11 @@ CREATE TABLE job_data (
     employee_residence VARCHAR(100),
     remote_ratio INT CHECK (remote_ratio IN (0, 50, 100)),
     company_location VARCHAR(100),
-    company_size CHAR(1) CHECK (company_size IN ('S', 'M', 'L'))
+    company_size CHAR(1) CHECK (company_size IN ('S', 'M', 'L')),
+    load_time TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
-ALTER TABLE job_data
+ALTER TABLE staging_data
 ALTER COLUMN work_year TYPE INT USING work_year::INT,
 ALTER COLUMN experience_level TYPE VARCHAR(50) USING experience_level::VARCHAR(50),
 ALTER COLUMN employment_type TYPE VARCHAR(50) USING employment_type::VARCHAR(50),
@@ -23,6 +24,5 @@ ALTER COLUMN salary_in_usd TYPE DECIMAL(10, 2) USING salary_in_usd::DECIMAL(10, 
 ALTER COLUMN employee_residence TYPE VARCHAR(100) USING employee_residence::VARCHAR(100),
 ALTER COLUMN remote_ratio TYPE INT USING remote_ratio::INT,
 ALTER COLUMN company_location TYPE VARCHAR(100) USING company_location::VARCHAR(100),
-ALTER COLUMN company_size TYPE CHAR(1) USING company_size::CHAR(1); 
-
-
+ALTER COLUMN company_size TYPE CHAR(1) USING company_size::CHAR(1),
+ALTER COLUMN load_time TYPE TIMESTAMP USING load_time::TIMESTAMP;
