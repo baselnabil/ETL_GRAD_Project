465dc0a045cb4136d7374ebb1e26585bfe0546ef
youssefyr <siyrr01@gmail.com>
2024-10-08 16:12:08 +0300

    Base doc server

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+import mdx from '@next/mdx';
+
+const withMDX = mdx();
+ 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Configure `pageExtensions` to include MDX files
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  // Optionally, add any other Next.js config below
+}
+ 
+export default withMDX(nextConfig);
+
