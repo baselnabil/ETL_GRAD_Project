904511d73e96b691fe15ab185b74db0d8f54abf8
youssefyr <siyrr01@gmail.com>
2024-10-16 17:42:36 +0300

    addded an export by mistake

--- src/app/docs/[...slug]/page.tsx
+++ src/app/docs/[...slug]/page.tsx
@@ -29,7 +29,7 @@ interface ExtractHeadingsOptions {
   slugFn?: (title: string) => string;
 }
 
-export function extractHeadings(
+function extractHeadings(
   content: string,
   options?: ExtractHeadingsOptions
 ): Array<Heading> {
